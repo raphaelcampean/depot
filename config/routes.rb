29b36b24e51f1e68f8fdf9c3d7dcbb335edb9656
Raphael Campean <raphaelcampean76@gmail.com>
2024-10-02 10:39:58 -0300

    adds line item

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
   resources :products
   root 'store#index', as: 'store_index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
